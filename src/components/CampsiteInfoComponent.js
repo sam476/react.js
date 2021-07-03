3157d5cdfbd0f60ba9d247bcf1a75e4823a6ab68
Sam Polite <samgel939@gmail.com>
2021-07-02 20:55:39 -0500

    workshop assignJuly2nd

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,99 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false,
+      rating: '',
+      author: '',
+      text: '',
+      touched: {
+        rating: false,
+        author: false,
+        text: false
+      }
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleSubmit(values) {
+    console.log("Current state is: " + JSON.stringify(values));
+    alert("Current state is: " + JSON.stringify(values));
+    this.toggleModal();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+
+        <Button className="fa-pencil fa-lg submitSize" outline onClick={this.toggleModal}>Submit Comment</Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+              <div className="form-group">
+                <Label htmlFor="rating">Rating</Label>
+                <Control.select model=".rating" id="rating" name="rating"
+                  placeholder="1"
+                  className="form-control"
+                ><option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option></Control.select>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="author">Author</Label>
+                <Control.text model=".author" id="author" name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                  validators={{
+                    required,
+                    minLength: minLength(2),
+                    maxLength: maxLength(15)
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  component="div"
+                  messages={{
+                    required: 'Required',
+                    minLength: 'Must be at least 2 characters',
+                    maxLength: 'Must be 15 characters or less'
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <Label htmlFor="text">Comment</Label>
+                <Control.textarea model=".text" id="text" name="text"
+                  rows="6" className="form-control" />
+              </div>
+              <Button type="submit" value="submit" color="primary">Submit</Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </React.Fragment>);
+  }
+}
 
 function RenderCampsite({ campsite }) {
   return (<div className="col-md-5 m-1"><Card>
@@ -25,6 +117,7 @@ function RenderComments({ comments }) {
         </p>
         </div>);
       })}
+      <CommentForm>Submit Commit</CommentForm>
     </div>);
   } else {
     return (<div></div>);
